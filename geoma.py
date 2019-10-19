c4c89605e525474ded18192c1aa6e078aad1eb04
JokeNik <37951421+JokeNik@users.noreply.github.com>
2019-10-19 10:35:57 +0300

    Update geoma.py

--- geoma.py
+++ geoma.py
@@ -7,22 +7,23 @@ PI = 3.14159265359
 def torad(x):
     return x * PI / 180
 
-alpha = int(input())
-twobeta = int(input());
-twogama = int(input());
-h = int(input());
-phi = int(input());
-x = int(input());
-y = int(input());
-beta = twobeta / 2
-gama = twogama / 2
+alpha = int(input()) #Угол наклона дрона относительно вертикальной линии против часовой стрелки
+twobeta = int(input());#Градусный диапазон по горизонтали который видит дрон
+twogama = int(input());#Градусный дипазон по вертикали который видит дрон
+h = int(input());#высота надо объектом
+phi = int(input());#угол поворота дрона относительно прямоугольный координат
+x = int(input());#х координата дрона
+y = int(input());#у координата дрона
+beta = twobeta / 2#узнаем половинные углы
+gama = twogama / 2#узнаем половинные углы
 
+#Переводим в радианы
 alpha = torad(alpha)
 beta =  torad(beta)
 gama = torad(gama)
 phi = torad(phi)
 
-
+#Считаеаем координаты трапеции
 x1 = (x + math.tan(alpha - gama) * h) * math.cos(-phi) - (y - h // (math.cos(alpha - gama) * math.tan(beta))) * math.sin(-phi)
 y1 = (x + math.tan(alpha - gama) * h) * math.sin(-phi) + (y - h // (math.cos(alpha - gama) * math.tan(beta))) * math.cos(-phi)
 x2 = (x + math.tan(alpha - gama) * h) * math.cos(-phi) - (y + h // (math.cos(alpha - gama) * math.tan(beta))) * math.sin(-phi)
@@ -34,7 +35,9 @@ x4 = (x + math.tan(alpha + gama) * h) * math.cos(-phi) - (y + h // (math.cos(alp
 y4 = (x + math.tan(alpha + gama) * h) * math.sin(-phi) + (y + h // (math.cos(alpha + gama) * math.tan(beta))) * math.cos(-phi)
 
 #print(alpha, beta, gama, phi, sep = " ")
+#Вывод вершин трапеции в порядке обхода
 print(x1, y1, sep = " ")
 print(x2, y2, sep = " ")
 print(x4, y4, sep = " ")
 print(x3, y3, sep = " ")
+
